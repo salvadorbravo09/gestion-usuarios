18c7cbebc030cc5a72266d5caebcea5af9d49ce9
Salvador Bravo <sa.bravo0901@gmail.com>
2025-02-20 18:04:11 -0300

    feat: create server

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-import { hello } from '#Lib/hello.js';
+import httpServer from './config/http.js';
+import config from './config/env.js';
 
-hello();
+const bootstrap = () => {
+  httpServer.listen(config.PORT, () => {
+    console.log(`Server running on port ${process.env.PORT}`);
+  });
+};
+
+bootstrap();
