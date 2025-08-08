b97503c38d7320a1ad019465317d0e3aa3b0eb21
Salvador Bravo <sa.bravo0901@gmail.com>
2025-08-07 23:55:09 -0400

    feat: se agregó soporte para paginación en UserRepository

--- src/main/java/com/sbravoc/gestion_usuarios/domain/user/repository/UserRepository.java
+++ src/main/java/com/sbravoc/gestion_usuarios/domain/user/repository/UserRepository.java
@@ -1,10 +1,14 @@
 package com.sbravoc.gestion_usuarios.domain.user.repository;
 
 import com.sbravoc.gestion_usuarios.domain.user.model.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends CrudRepository<User, Long> {
 
+    Page<User> findAll(Pageable pageable);
 }
