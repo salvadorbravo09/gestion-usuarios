8a838eba833297d93d9de7e874b35e88bcbcaa9a
Salvador Bravo <sa.bravo0901@gmail.com>
2025-08-10 16:38:56 -0400

    feat: se creó la entidad Role con atributos y mapeo a la tabla roles

---
+++ src/main/java/com/sbravoc/gestion_usuarios/domain/user/model/Role.java
@@ -0,0 +1,31 @@
+package com.sbravoc.gestion_usuarios.domain.user.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false, unique = true)
+    private String name;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+}
