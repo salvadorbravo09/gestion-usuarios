b624c4c56c045d234185dc179016d7319aed5d14
Salvador Bravo <sa.bravo0901@gmail.com>
2025-08-10 00:07:53 -0400

    feat: se agregó método para buscar usuario por nombre de usuario en UserRepository

--- src/main/java/com/sbravoc/gestion_usuarios/domain/user/repository/UserRepository.java
+++ src/main/java/com/sbravoc/gestion_usuarios/domain/user/repository/UserRepository.java
@@ -7,8 +7,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends CrudRepository<User, Long> {
 
     Page<User> findAll(Pageable pageable);
+
+    Optional<User> findByUsername(String username);
 }
